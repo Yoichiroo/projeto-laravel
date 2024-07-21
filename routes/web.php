2ab111e22d28335ba84b2b4406004da021e4e42c
Mat-in <109477475+Yoichiroo@users.noreply.github.com>
2024-07-21 17:25:54 -0300

    criado os controllers e algumas rotas do crud

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::post('/users/{user}', [UserController::class, 'store'])->name('users.store');
